43fa76286bf55b12a449297a54c4a507d7f0bd4b
MBappy-404 <muhammadbappy989@gmail.com>
2025-05-03 15:03:43 +0600

    dynamic movie page

---
+++ src/components/redux/features/content/contentApi.ts
@@ -0,0 +1,68 @@
+import { baseApi } from "../../api/baseApi";
+
+const contentApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    createContent: builder.mutation({
+      query: (data) => ({
+        url: "/content",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["content"],
+    }),
+
+    getAllContent: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+
+        // if (args) {
+        //   args.forEach((item: any) => {
+        //     params.append(item?.name, item?.value as string);
+        //   });
+        // }
+
+        return {
+          url: "/content",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["content"],
+      // transformResponse: (response: TResponseRedux<TSemester[]>) => {
+      //   return {
+      //     data: response.data,
+      //     meta: response.meta,
+      //   };
+      // },
+    }),
+    deleteContent: builder.mutation({
+      query: (params) => ({
+        url: `/content/${params?.productId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["content"],
+    }),
+    getContent: builder.query({
+      query: (params) => ({
+        url: `/content/${params?.productId}`,
+        method: "GET",
+      }),
+    }),
+    updateContent: builder.mutation({
+      query: ({ productId, formData }) => ({
+        url: `/content/${productId}`,
+        method: "PUT",
+        body: formData,
+      }),
+      invalidatesTags: ["content"],
+    }),
+  }),
+});
+
+export const {
+    useCreateContentMutation,
+    useGetAllContentQuery,
+    useDeleteContentMutation,
+    useGetContentQuery,
+    useUpdateContentMutation,
+} = contentApi;
