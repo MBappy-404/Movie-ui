6a5d8eb44b40320bf83937e0ad4f4f78baaed9b6
sopnilali <sopnilali24@gmail.com>
2025-05-06 18:07:36 +0600

    cloudinary problem fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizeCss: false, // Disable experimental CSS optimization
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'res.cloudinary.com',
       },
     ],
   },
