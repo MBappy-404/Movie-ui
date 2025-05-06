da07307f6dcfc83435e11b2cfa9aa47e0931b107
sopnilali <sopnilali24@gmail.com>
2025-05-06 18:11:10 +0600

    image issue try to fix

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/images/**',
+      },
     ],
   },
 };
