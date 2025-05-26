1a8def9b679e9dbf7f383ccaaece342ba9f7e6ac
sopnilali <sopnilali24@gmail.com>
2025-05-27 03:26:17 +0600

    update my review section

--- src/components/Dashboardlayout/Reviews/UserReviews.tsx
+++ src/components/Dashboardlayout/Reviews/UserReviews.tsx
@@ -62,7 +62,7 @@ const UserReviews = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
       {userReviews.map((review: any) => (
         <div
           key={review.id}
